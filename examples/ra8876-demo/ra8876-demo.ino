86097cb0908e2a7e2ea630fe953b128fa41aa354
Andrew Wendt <andy@putergeek.com>
2017-02-04 23:23:09 -0800

    Add missing symbols to demo program.

--- examples/ra8876-demo/ra8876-demo.ino
+++ examples/ra8876-demo/ra8876-demo.ino
@@ -193,6 +193,8 @@ void textTest()
   tft.print("Symbols: ");
   tft.putChars((const uint8_t *) "\x00\x01\x02\x03\x04\x05\x06\x07", 8);
   tft.putChars((const uint8_t *) "\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F", 8);
+  tft.putChars((const uint8_t *) "\x10\x11\x12\x13\x14\x15\x16\x17", 8);
+  tft.putChars((const uint8_t *) "\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F", 8);
 
   uint32_t elapsedtime = millis() - starttime;
   Serial.print("Text test took "); Serial.print(elapsedtime); Serial.println(" ms");
